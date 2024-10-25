f9e34391d150c6a96231036dc0daf6e15245b5e4
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-10-25 15:49:52 -0400

    [create] wishlist model

--- steambotapi/wishlists/models.py
+++ steambotapi/wishlists/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Wishlist(models.Model):
+  name = models.CharField(max_length=400)
+  language = models.CharField(max_length=400)
+  price = models.CharField(max_length=20)
