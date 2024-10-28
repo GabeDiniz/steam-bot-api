a27722438c9b3fd99c872181d2a6c2e669680238
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-10-28 19:10:47 -0400

    [add] wishlist model to the admin area

--- steambotapi/wishlists/models.py
+++ steambotapi/wishlists/models.py
@@ -4,3 +4,6 @@ class Wishlist(models.Model):
   name = models.CharField(max_length=400)
   language = models.CharField(max_length=400)
   price = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
